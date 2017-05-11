b91d59d569ed238718171e89b8c8484eb32a78ff
samgar01 <samugarcia9696@gmail.com>
2017-05-11 09:14:09 +0200

    cerrada etiqueta html y añadido lineas de adaptación móvil

--- js/emusic.js
+++ js/emusic.js
@@ -9,3 +9,19 @@ var contentCurrent = $(".ui-content").outerHeight() - $(".ui-content").height();
 var content = screen - header - footer - contentCurrent;
 
 $(".ui-content").height(content);
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
+            document.addEventListener("deviceready", this.onDeviceReady, false);
+        } else {
+            this.onDeviceReady();
+        }
+    },
+
+    onDeviceReady: function() {
+        // We will init / bootstrap our application here
+    },
+};
+app.initialize();
